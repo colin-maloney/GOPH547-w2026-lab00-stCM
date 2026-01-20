200d6273d7246583ce3f551c013f9dd7c7f3c563
Colin.Maloney <colin.maloney@ucalgary.ca>
2026-01-19 21:22:17 -0700

    finshed code from driver.py

--- src/goph547lab00/driver.py
+++ src/goph547lab00/driver.py
@@ -10,7 +10,13 @@ array_3_sum = np.sum(array_3)
 
 array_4 = [[5, 7, 2],[1, -2, 3], [4, 4, 4]] 
 
-array_5 = np.identity(3) 
+array_5 = np.identity(3)  
+
+array_6 = np.multiply(array_4, array_5) 
+
+dot_product = np.dot(array_4, array_5) 
+
+cross_product = np.cross(array_4, array_5)
 
 print("Array 1:\n", array_1)
 print("Array 2:\n", array_2)
@@ -18,3 +24,6 @@ print("Array 3:\n", array_3)
 print("Sum of Array 3:", array_3_sum)
 print("Array 4:\n", array_4)
 print("Array 5:\n", array_5) 
+print("Array 6 (Element-wise multiplication of Array 4 and Array 5):\n", array_6)
+print("Dot Product of Array 4 and Array 5:\n", dot_product) 
+print("Cross Product of Array 4 and Array 5:\n", cross_product)
